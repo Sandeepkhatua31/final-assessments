c212adea82ae289fa021ef5a3bf2bfe1631c7e22
Sandeep <Sk3264537@gmail.com>
2022-03-31 17:03:56 +0530

    phase 4 Assisted project

---
+++ phase 4/assisted project/Session 4/first.js
@@ -0,0 +1,6 @@
+var num1;
+num1 = 23;
+var fname = "Rishi";
+var lname = "Kulkarni";
+console.log("Number: " + num1);
+console.log(fname + "." + lname);
---
+++ phase 4/assisted project/Session 4/first.ts
@@ -0,0 +1,9 @@
+let num1:number;
+
+num1=23;
+
+let fname:string="Rishi";
+let lname:string="Kulkarni";
+
+console.log("Number: "+ num1);
+console.log(fname+"."+lname);
